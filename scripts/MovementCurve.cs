ed46ae536a0915c4028a002f360c3d1115e03a64
slackers <47892117+slackers123@users.noreply.github.com>
2021-03-05 14:35:19 +0100

    Create MovementCurve.cs
    Added the scriptable object "Movement Curves"

---
+++ scripts/MovementCurve.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Movement Curve", menuName = "Inventory/MovementCurve")]
+public class MovementCurve : ScriptableObject
+{
+    public string name = "New Movement Curve";
+    public float timeScale = 1;
+    public AnimationCurve Curve = null;
+
+    public float Evaluate(float t)
+    {
+        return Curve.Evaluate(t) * timeScale;
+    }
+}
